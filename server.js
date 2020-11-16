9db32aa63cc639ea6c574cdf28d2282a7b755f5f
dipeshkoirala <dipeshkoirala@gmail.com>
2020-11-16 18:44:54 -0500

    resolving issues in package.json

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 
 const express=require('express')
-const helmet=require("helmet")
+// const helmet=require("helmet")
 const cors=require("cors")
 // const db= require('./data')
 // const bodyParser=require('body-parser')
-const session = require("express-session");
+// const session = require("express-session");
 
 const welcomeRouter=require("./router/welcome-router")
 const authRouter=require("./router/auth-router")
 
 const server=express()
-server.use(helmet());
+// server.use(helmet());
 server.use(cors());
 
 server.use(express.json())
 
-server.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.JWT_SECRET || "Secret word",
-  })
-);
+// server.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.JWT_SECRET || "Secret word",
+//   })
+// );
 // server.use(logger)
 // server.use(helmet())
 
