3eb5d6ede2b53ffc95bb1e0acee373c4b59f609d
furno west <furnowest57@gmail.com>
2020-11-17 19:17:11 -0800

    added restrict

--- router/user-router.js
+++ router/user-router.js
@@ -4,9 +4,9 @@ const jwt = require("jsonwebtoken")
 const db = require ("../model/user-model")
 const { restrict } = require("../middleware/restricted")
 
-const router = express.Router()
 
-router.get("/users", async (req, res, next) => {
+
+router.get("/users", restrict, async (req, res, next) => {
 	try {
         const users = await db.find()
         res.json(users)
